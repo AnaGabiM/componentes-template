c5668c57143ddd17c3bb2b87ede12c7d6e0d8040
AnaGabiM <ana.agma98@gmail.com>
2023-02-23 23:26:53 -0300

    Ex.Componentes

--- template/src/App.js
+++ template/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import Header from "./componentes/header";
+import Footer from "./componentes/footer";
 
 export default function App() {
-  
 
   return (
     <div className="App">
-      
-     <h1>Aula - Componentes React</h1>
+     <Header />
+     <Footer />
     </div>
   );
 }
---
+++ template/src/componentes/redesSociais.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function RedesSociais() {
+    return (
+        <div>
+            <ul>
+                <li>Facebook</li>
+                <li>instagram</li>
+            </ul>
+        </div>
+    );
+}
+
+export default RedesSociais
